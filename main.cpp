dfd6b264ba40bf5d1f6085a21bdae2113a0a2cc6
burak balta <burakkbalta@gmail.com>
2022-03-16 01:30:07 +0300

    first commit

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@
 
 int main()
 {
-    std::tuple tup {15, 20.7, "tuple_print"};
+    std::tuple tup {16, 20.7, "tuple_print"};
     using namespace bb::tuple_helper;
     std::cout << tup << "\n";
 
-    auto val = isExistInTuple(tup, 15);
+    auto val = isExistInTuple(tup, 16);
     std::cout << std::boolalpha << val << "\n";
     
     val = isExistInTuple(tup, 20.7);
